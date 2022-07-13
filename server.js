2a605e78019359f35a0b3649a9067eea45abde2c
Collin Blank <collin@Collins-Mac-mini.local>
2022-07-13 18:20:09 -0400

    created api music tour

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 // DEPENDENCIES
 const express = require('express')
 const app = express()
+const { Sequelize } = require('sequelize')
 
 // CONFIGURATION / MIDDLEWARE
 require('dotenv').config()
 app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false }))  
 
 // ROOT
 app.get('/', (req, res) => {
@@ -13,6 +14,9 @@ app.get('/', (req, res) => {
         message: 'Welcome to the Tour API'
     })
 })
+
+const bandsController = require('./controllers/bands_controller')
+app.use('/bands', bandsController)
 
 // LISTEN
 app.listen(process.env.PORT, () => {
